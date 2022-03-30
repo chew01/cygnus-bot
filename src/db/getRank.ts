0c63a5721b7b8c67d4871938c9dd7aba92999e58
chew01 <chewjh19@gmail.com>
2022-03-31 00:35:29 +0800

    Switched from limit 10 to limit 5 for leaderboard

--- src/db/getRank.ts
+++ src/db/getRank.ts
@@ -1,11 +1,13 @@
 import pool from './index';
 import { RankingData } from '../types/rank';
+import log from '../utils/logger';
 
 export async function getCharRank(character: string): Promise<RankingData> {
   const client = await pool.connect();
   const query = 'SELECT * FROM ranking WHERE LOWER(charactername) = LOWER($1)';
   const characterEncoded = encodeURIComponent(character);
   try {
+    log.info(`[DB] Query: Get character rank of ${character}`);
     const res = await client.query(query, [characterEncoded]);
     return res.rows[0];
   } finally {
@@ -13,4 +15,14 @@ export async function getCharRank(character: string): Promise<RankingData> {
   }
 }
 
-export default getCharRank;
+export async function getWorldRank(world: string): Promise<RankingData[]> {
+  const client = await pool.connect();
+  const query = 'SELECT * FROM ranking WHERE worldname = $1 LIMIT 5';
+  try {
+    log.info(`[DB] Query: Get world rankings of ${world}`);
+    const res = await client.query(query, [world]);
+    return res.rows;
+  } finally {
+    client.release();
+  }
+}
