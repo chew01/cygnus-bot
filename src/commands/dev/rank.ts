c2319a2e5b4957664458237b6c4ef89ede1b7c31
chew01 <chewjh19@gmail.com>
2022-03-31 00:37:09 +0800

    Added leaderboard embed and creator function

--- src/commands/dev/rank.ts
+++ src/commands/dev/rank.ts
@@ -1,17 +1,24 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
-import { CommandInteraction, MessageEmbed } from 'discord.js';
+import {
+  CommandInteraction,
+  Message, MessageActionRow, MessageButton, MessageComponentInteraction, MessageEmbed,
+} from 'discord.js';
 import { SlashCommand } from '../../types/command';
-import { getCharRank } from '../../db/getRank';
-import { formatBigint } from '../../utils/helpers';
+import { getCharRank, getWorldRank } from '../../db/getRank';
+import { formatBigint, rankToEmoji, serverToEmoji } from '../../utils/helpers';
 import { CDN_URL } from '../../config';
+import { RankingData } from '../../types/rank';
+import log from '../../utils/logger';
+
+type rankingType = 'overallrank' | 'worldrank'
 
 async function playerRank(interaction: CommandInteraction): Promise<void> {
   const character = interaction.options.getString('character');
   if (!character) return interaction.reply({ content: 'No character was specified!', ephemeral: true });
   const data = await getCharRank(character);
   if (!data) return interaction.reply({ content: 'Character does not exist!', ephemeral: true });
+
   const charEmbed = new MessageEmbed()
-    .setAuthor({ name: 'Cygnus', iconURL: 'https://i.imgur.com/LNZkA9g.jpg' })
     .setThumbnail(`${CDN_URL}${data.characterimgurl.substring(38)}`)
     .setTitle(`${data.charactername} - ${data.worldname}`)
     .addFields(
@@ -35,6 +42,65 @@ async function playerRank(interaction: CommandInteraction): Promise<void> {
   return interaction.reply({ embeds: [charEmbed] });
 }
 
+async function createLeaderboard(
+  data: RankingData[],
+  ranking: rankingType,
+  thumbnail: string,
+  title: string,
+) {
+  const fields = data.map((charData) => ({
+    name: `${rankToEmoji(charData[ranking])} ${charData.charactername} - Lv. ${charData.level} ${charData.jobname}`,
+    value: `> Global Rank #${charData.overallrank} | World Rank #${charData.worldrank}
+        > World Legion Rank #${charData.legionrank ?? 'Unavailable'}`,
+  }));
+
+  return new MessageEmbed()
+    .setThumbnail(thumbnail)
+    .setTitle(title)
+    .addFields(fields);
+}
+
+async function worldLeaderboard(interaction: CommandInteraction): Promise<void> {
+  const world = interaction.options.getString('world');
+  if (!world) return interaction.reply({ content: 'No world was specified!', ephemeral: true });
+  const data = await getWorldRank(world);
+  if (!data) return interaction.reply({ content: 'World does not exist!', ephemeral: true });
+
+  const worldEmbed = await createLeaderboard(
+    data,
+    'worldrank',
+    'https://i.imgur.com/GC5JkFT.png',
+    `Leaderboard - ${world} ${serverToEmoji(world)}`,
+  );
+  const prevBtn = new MessageButton()
+    .setCustomId('prevbtn')
+    .setLabel('Previous')
+    .setEmoji('arrow_backward')
+    .setStyle('PRIMARY');
+  const nextBtn = new MessageButton()
+    .setCustomId('nextbtn')
+    .setLabel('Next')
+    .setEmoji('arrow_forward')
+    .setStyle('PRIMARY');
+  const gallery = new MessageActionRow()
+    .addComponents(prevBtn, nextBtn);
+
+  if (!interaction.deferred) await interaction.deferReply();
+  const curPage = await interaction.editReply({
+    embeds: [worldEmbed],
+    components: [gallery],
+  }) as Message;
+
+  const filter = (i: MessageComponentInteraction) => i.user.id === interaction.user.id
+        && (i.customId === 'prevbtn' || i.customId === 'nextbtn');
+  const collector = curPage.createMessageComponentCollector({ filter, time: 30000 });
+
+  collector.on('collect', async (i) => {
+    log.info(i.user.id);
+  });
+  return log.info('placeholder');
+}
+
 const rankCommand: SlashCommand = {
   data: new SlashCommandBuilder()
     .setName('ms')
@@ -48,15 +114,23 @@ const rankCommand: SlashCommand = {
         .setRequired(true)))
     .addSubcommand((subcommand) => subcommand
       .setName('w')
-      .setDescription('Finds the ranking data for a Maplestory world.'))
+      .setDescription('Finds the ranking data for a Maplestory world.')
+      .addStringOption((option) => option
+        .setName('world')
+        .setDescription('The Maplestory world to find rankings for.')
+        .setRequired(true)
+        .addChoice('Scania', 'Scania')
+        .addChoice('Bera', 'Bera')
+        .addChoice('Aurora', 'Aurora')
+        .addChoice('Elysium', 'Elysium')
+        .addChoice('Reboot (NA)', 'Reboot (NA)')))
     .addSubcommand((subcommand) => subcommand
       .setName('j')
       .setDescription('Finds the ranking data for a Maplestory job.')),
   async execute(interaction: CommandInteraction) {
     const subcommand = interaction.options.getSubcommand();
-    if (subcommand === 'p') {
-      return playerRank(interaction);
-    }
+    if (subcommand === 'p') return playerRank(interaction);
+    if (subcommand === 'w') return worldLeaderboard(interaction);
     return interaction.reply({ content: 'Invalid subcommand!', ephemeral: true });
   },
 };
