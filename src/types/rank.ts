c2ce563e683b26670e78b43e3d5582056a466f15
chew01 <chewjh19@gmail.com>
2022-03-31 23:59:53 +0800

    Added cache layer instead of pure db persistence

--- src/types/rank.ts
+++ src/types/rank.ts
@@ -5,10 +5,10 @@ export type RankingData = {
     exp: bigint
     jobname: string,
     worldname: string,
-    worldid: number
     overallrank: number,
     legionlevel: number | null,
     raidpower: bigint | null,
     legionrank: number | null,
     worldrank: number,
+    index?: number
 }
